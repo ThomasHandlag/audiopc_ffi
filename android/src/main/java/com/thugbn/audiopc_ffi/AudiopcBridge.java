39cb6b9d693fae23ba2e4cfaa87a016f96313a51
Thomas Handlag <truongthuongvan203@gmail.com>
2026-04-28 16:43:28 +0700

    refactor: change android package name

---
+++ android/src/main/java/com/thugbn/audiopc_ffi/AudiopcBridge.java
@@ -0,0 +1,17 @@
+package com.example.audiopc_ffi;
+
+import android.content.Context;
+
+public final class AudiopcBridge {
+    static {
+        System.loadLibrary("audiopc_ffi");
+    }
+
+    private AudiopcBridge() {}
+
+    public static void init(Context context) {
+        nativeInit(context.getApplicationContext());
+    }
+
+    private static native void nativeInit(Context context);
+}
